c228ff4bd71c807de61d38681801382cfbae131f
ueno <radaokuno2525@gmail.com>
2022-11-30 18:48:37 +0900

    [update,delete]

--- scripts/main.ts
+++ scripts/main.ts
@@ -1,26 +1,7 @@
-import { Player, world, EntityInventoryComponent, Vector } from "@minecraft/server";
-import { showMainMenu } from "./Form/ContainerMenu/index";
-import { Schedule } from "./TimeHandler/TimeHandler";
-world.events.beforeItemUseOn.subscribe((ev) => {
-    const { blockLocation, source, item } = ev;
-    const block = source.dimension.getBlock(blockLocation);
-    if (block.typeId === "minecraft:chest" || block.typeId === "minecraft:brewing_stand") {
-        ev.cancel = true;
-        showMainMenu(<Player>source);
-    }
+import { world } from "@minecraft/server";
+import { greet } from "./function";
+
+world.events.beforeChat.subscribe((ev) => {
+    greet(ev.sender);
+    console.log(ev.message);
 });
-world.events.blockBreak.subscribe(({ player }) => {
-    const y = Schedule.repeat(tell, 20, -1, "a", player)
-        .then(() => {
-            world.say("ok" + `${new Date().getTime()}`);
-        })
-        .then(() => {
-            world.say("i");
-        });
-    Schedule.delay(() => {
-        y.cancel();
-    }, 60);
-});
-function tell(message: string, target: Player) {
-    target.tell(message + `${new Date().getTime()}`);
-}
